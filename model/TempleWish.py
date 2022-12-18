910f2f1b54cab3442eb0c597c6a0a02c77345131
wangpwm <wangpwm@yonyou.com>
2022-12-18 15:09:03 +0800

    nayin

--- model/TempleWish.py
+++ model/TempleWish.py
@@ -4,7 +4,7 @@ from collections import Counter
 
 from base.Paipan import Paipan
 from model.EightChar import EightChar
-from model.Nayin import na_yin
+from model.Nayin import na_yin_dict
 from model.TenGodTable import VigorousWeakTable, FiveElementTable, TenGodTrunkTable, TenGodBranchTable, SheepBladeTable
 
 
@@ -22,8 +22,24 @@ class TempleWish:
         self.branchTable = TenGodBranchTable()
         self.sheepBladeTable = SheepBladeTable()
         self.vigorousWeakTable = VigorousWeakTable()
+        self.chang_sheng = None
+        self.ten_god = None
+        self.na_yin = None
 
     def exec_analyze(self):
+        self.chang_sheng = [self.vigorousWeakTable.getResult(self.eightChar.dayTrunk + self.eightChar.yearBranch),
+                            self.vigorousWeakTable.getResult(self.eightChar.dayTrunk + self.eightChar.monthBranch),
+                            self.vigorousWeakTable.getResult(self.eightChar.dayTrunk + self.eightChar.dayBranch),
+                            self.vigorousWeakTable.getResult(self.eightChar.dayTrunk + self.eightChar.hourBranch)]
+        self.ten_god = [self.branchTable.getResult(self.eightChar.yearTrunk + self.eightChar.yearBranch),
+                        self.branchTable.getResult(self.eightChar.monthTrunk + self.eightChar.monthBranch),
+                        self.branchTable.getResult(self.eightChar.dayTrunk + self.eightChar.dayBranch),
+                        self.branchTable.getResult(self.eightChar.hourTrunk + self.eightChar.hourBranch)]
+        self.na_yin = []
+        for item in self.eightWord:
+            self.na_yin.append(na_yin_dict[item])
+
+    def exec_analyze_match(self):
         trunk = self.eightChar.trunk
         branch = self.eightChar.branch
         eightChar = self.eightChar.eightChar
@@ -74,11 +90,7 @@ class TempleWish:
         print("-" * 140)
         print(
             "{:^28s}{:^28s}{:^28s}{:^28s}{:^28s}"
-            .format('十神', self.branchTable.getResult(self.eightChar.yearTrunk + self.eightChar.yearBranch),
-                    self.branchTable.getResult(self.eightChar.monthTrunk + self.eightChar.monthBranch),
-                    self.branchTable.getResult(self.eightChar.dayTrunk + self.eightChar.dayBranch),
-                    self.branchTable.getResult(self.eightChar.hourTrunk + self.eightChar.hourBranch)
-                    ))
+            .format('十神', self.ten_god[0], self.ten_god[1], self.ten_god[2], self.ten_god[3]))
         print("{:^28s}{:^28s}{:^28s}{:^28s}{:^28s}"
               .format('天干', self.eightChar.yearTrunk, self.eightChar.monthTrunk, self.eightChar.dayTrunk,
                       self.eightChar.hourTrunk))
@@ -92,19 +104,10 @@ class TempleWish:
 
         print(
             "{:^28s}{:^28s}{:^28s}{:^28s}{:^28s}"
-            .format('十二长生',
-                    self.vigorousWeakTable.getResult(
-                        self.eightChar.dayTrunk + self.eightChar.yearBranch),
-                    self.vigorousWeakTable.getResult(
-                        self.eightChar.dayTrunk + self.eightChar.monthBranch),
-                    self.vigorousWeakTable.getResult(
-                        self.eightChar.dayTrunk + self.eightChar.dayBranch),
-                    self.vigorousWeakTable.getResult(
-                        self.eightChar.dayTrunk + self.eightChar.hourBranch)
-                    ))
+            .format('十二长生', self.chang_sheng[0], self.chang_sheng[1], self.chang_sheng[2], self.chang_sheng[3]))
         print("{:^28s}".format('纳音'), end=' ')
-        for item in self.eightWord:
-            print("{:^26s}".format(na_yin[item]), end=' ')
+        for item in self.na_yin:
+            print("{:^26s}".format(item), end=' ')
         print()
         print("-" * 140)
 
@@ -112,7 +115,8 @@ class TempleWish:
 if __name__ == '__main__':
     pai_pan = Paipan()
     eight_char = pai_pan.get_eight_char('甘肃省', '兰州市', "2022-12-18", "12:2:41")
-    #  a = EightChar("辛未 辛卯 乙酉 戊寅")
+    print(eight_char)
+    # a = TempleWish("辛未 辛卯 乙酉 戊寅")
     a = TempleWish("戊寅 癸亥 壬戌 丙午")
 
     # a = EightChar("辛未 辛卯 乙酉 丁丑")
@@ -120,5 +124,5 @@ if __name__ == '__main__':
     # a = EightChar("乙卯 丙戌 癸酉 丙辰")
     # a = EightChar("辛丑 甲午 丙申 壬辰")
     # a = EightChar("甲子 丁卯 丙申 丁酉")
-    a.exec_analyze_print()
     a.exec_analyze()
+    a.exec_analyze_print()
